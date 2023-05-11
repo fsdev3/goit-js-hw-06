b6aa533d06030c84f80b97b863f533e4c9a8aee7
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-05-11 13:45:16 +0200

    t1 z1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+// z-1
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach(item => {
+  const categoryName = item.querySelector('h2').textContent;
+  const categoryElQuantity = item.querySelectorAll('li').length;
+
+  console.log(`Category: ${categoryName}\nElements: ${categoryElQuantity}`);
+});
+// z-2
